b0dedce6307f82753448a1c8633b4a1a7a0ddc36
Regie Daquioag <regiebrick@gmail.com>
2015-10-09 16:57:32 -0700

    I put in the menu() function

--- driver.cpp
+++ driver.cpp
@@ -20,6 +20,22 @@ double discount(char choice, int people);
 void receipt(double total, int people, double discount);
 
 int main()
-{
+{	
 	return 0;
+}
+
+char menu()
+{
+    char choice;
+    
+    cout << "       The High Adventure Travel Package:\n" << endl;
+    cout << "Enter in '1' for Devil’s Courthouse Adventure Weekend" << endl;
+    cout << "Enter in '2' for Scuba Bahama" << endl;
+    cout << "Enter in '3' for Sky Dive Colorado" << endl;
+    cout << "Enter in '4' for Barron Cliff Spelunk" << endl;
+    
+    cin >> choice;
+    
+    return choice;
+    
 }
