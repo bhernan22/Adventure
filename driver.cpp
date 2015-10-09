e7b20048357d826f40a1d534cac7a3340fd9889b
Bianca Hernandez <biancakhernandez@student.hartnell.edu>
2015-10-09 11:35:14 -0700

    Added a driver file with prototype functions

---
+++ driver.cpp
@@ -0,0 +1,25 @@
+// Bianca Hernandez
+// Coleman Johnston
+// Chanel Aquino
+// Regie Daquioag
+// 
+//********************************************************
+
+#include <iostream> // cin/cout
+#include <cassert> // assert unit testing
+
+using namespace std;
+
+char menu();
+double climbing(int people);
+double scuba(int people);
+double skyDive(int people);
+double spelunk(int people);
+double discount(double baseCharge, int people);
+double discount(char choice, int people);
+void receipt(double total, int people, double discount);
+
+int main()
+{
+	return 0;
+}
