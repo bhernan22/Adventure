b3bf56c2655bf8ce569ca82c54a55e0fe36bd96f
Bianca Hernandez <biancakhernandez@student.hartnell.edu>
2015-10-13 12:49:14 -0700

    Final touches

--- driver.cpp
+++ driver.cpp
@@ -26,7 +26,7 @@ int main()
 	char choice;
 	double total, discountTotal;
 	
-	cout << 	"\nWelcome to the High Adventure Travel Agency!" << endl;
+	cout << 	"\n\tWelcome to the High Adventure Travel Agency!" << endl;
 	cout << "We offer four vacation packages for thrill-seeking customers!" << endl;
 	
 	cout <<  "\nHow many people will be participating in this trip? ";
@@ -44,7 +44,7 @@ int main()
 						break;
 		case '4': total = spelunk(people);
 						break;
-		default: cout << "You enter an invalid input. Please Try Again." << endl;
+		default: cout << "You enter an invalid input. Please Try Again."  << menu();
 	}
 	
 	discountTotal = discount(choice, people);
@@ -52,25 +52,158 @@ int main()
 	
 	return 0;
 
-}   // main()
+}   // end of main()
 
-//*******************************
 char menu()
 {
     char choice;
     
-    cout << "       The High Adventure Travel Package:\n" << endl;
+    cout << "\n       The High Adventure Travel Packages:\n" << endl;
     cout << "Enter in '1' for Devil’s Courthouse Adventure Weekend" << endl;
     cout << "Enter in '2' for Scuba Bahama" << endl;
     cout << "Enter in '3' for Sky Dive Colorado" << endl;
     cout << "Enter in '4' for Barron Cliff Spelunk" << endl << endl;
-    cout<< "Please select one of our packages.";
+    cout<< "Please select one of our packages: ";
     cin >> choice;
     
     return choice;   
-}   // menu()
+}   // end of menu()
 
-//*******************************
+double climbing(int people)
+{
+    double 
+        discountTotal = 0,
+        baseTotal, 
+        totalAmount,  
+        climbOptional, 
+        equipOptional;
+        
+    int 
+        peopleOptional, 
+        daysOptional;
+
+    const double 
+        BASE_CHARGE = 350, 
+        DISCOUNT_RATE = 0.10, 
+        CLIMB_INSTR = 100, 
+        EQUIP_RENT = 40,
+        NUM_DAYS = 3;
+    
+    cout 
+        << "\nThank you for choosing Devil's Courthouse Adventure Weekend!" << endl
+        << endl << "Listed below are our rates." << endl
+        << "\t⇨ Base Charge: \t\t\t$" << BASE_CHARGE << " per person" << endl
+        << "\t⇨ Climbing Instructions: \t$" << CLIMB_INSTR << " per person (optional)" << endl
+        << "\t⇨ Equipment Rental: \t\t$" << EQUIP_RENT << "/day per person (optional)" << endl << endl;
+    
+    // calculate the base total, before discount
+    baseTotal = BASE_CHARGE * people;
+    
+    // call discount() to calculate the discount
+    discountTotal = discount(BASE_CHARGE, people);
+    
+    cout 
+        << "You have [" << people << "] people in your party." << endl
+        << "Your Base Charge (before discount) is:\t$" << baseTotal << endl
+        << "Your Discount Total is:\t\t\t(-$" << discountTotal << ")" << endl;
+        
+        baseTotal -= discountTotal; // subtract discount total from the base total
+        totalAmount = baseTotal;    // update the total amount
+    
+        
+    cout << "Your Base Charge (after discount) is:\t$" << baseTotal << endl << endl;
+    
+    cout << "Enter the number of people who will need a climbing instructor: ";
+    cin >> peopleOptional;
+    
+    while (peopleOptional < 0 || peopleOptional > people)
+        // the people who need instruction (peopleOptional) should not exceed the number of people in the party
+    {  
+        cout 
+            << "\tError: You entered more people than are in your party. Please try again."<< endl
+            << "Enter the number of people who will need a climbing instructor: ";
+        cin >> peopleOptional;
+    }   // while()
+            
+    cout
+        << "\n[" << peopleOptional << "] people will need a climbing instructor." << endl 
+        << "$" << CLIMB_INSTR << " charge x [" << peopleOptional << "] people =" << endl;
+    
+    climbOptional = CLIMB_INSTR * peopleOptional;   // the optional climbing instruction charge is $100/person
+    
+    cout << "\t⇨ $" << climbOptional << " will be added to your current total of $" << totalAmount << "." << endl;
+    totalAmount += climbOptional;  // add the optional charge of climbing instructions to the totalCharge
+    
+    cout << "\t⇨ Your updated total is $" << totalAmount << "." << endl << endl;
+    
+    cout << "Enter the number of people who will need to rent equipment: ";
+    cin >> peopleOptional;
+    
+    while (peopleOptional < 0 || peopleOptional > people)
+        // the people who need instruction (peopleOptional) should not exceed the number of people in the party
+    {
+        cout 
+            << "\tError: You entered more people than are in your party. Please try again." << endl
+            << "Enter the number of people who will need to rent equipment: ";
+        cin >> peopleOptional;
+    }   // while()
+        
+    cout << "How many days will you be renting equipment? ";
+    cin >> daysOptional;
+        
+    while (daysOptional < 0 || daysOptional >  NUM_DAYS)
+        // the number of days needed for equipment (daysOptional) should not exceed the total number of days
+    {   cout 
+            << "\tError: You entered more days than this package allows. Please try again." << endl
+            << "How many days will you be renting equipment? ";
+        cin >> daysOptional;
+    }   // while()
+        
+    
+    cout << "\n$" << EQUIP_RENT << " charge x [" << peopleOptional << "] people x [" << daysOptional << "] days =" << endl;
+    equipOptional = EQUIP_RENT * peopleOptional * daysOptional; // the optional equipment rental charge is $40/day per person
+
+    cout << "\t⇨ $" << equipOptional << " will be added to your current total of $" << totalAmount << "." << endl;
+
+    totalAmount +=  equipOptional;  // add the optional charge of equipment rental, equipOptional, to the totalCharge
+    
+    cout << "\t⇨ Your updated total is $" << totalAmount << "." << endl;
+            
+    return totalAmount;    
+}   // end of climbing()
+
+double scuba(int people)
+{
+    const double PERSON_PRICE = 350;//price per person
+    const double INSTRUCRION_PRICE = 100;//price of scuba instruction
+    int students;//variable to hold the number of people who need scuba instruction
+    double baseDiscount = 0;//discount off of the base price (positive number!)
+       
+    cout << "\nThank you for choosing Scuba Bahama!\n" << endl;
+    do//check and make sure the amount of people who need instruction is <= the number of people going on the trip
+    {
+        cout << "How many people need diving lessons?\n";
+        cout << "(Those with prior experience may dive right in, while beginners should choose\n"; 
+        cout << "to take optional, but very affordable lessons.)\n";
+        cin >> students;
+    
+        if(students > people)
+        {
+            cout << "You have entered a number greater than the number of people in your party.\n";
+            cout << "Please enter a smaller number.\n\n";
+        }
+        else if(students < 0)
+        {
+            cout << "Please enter a positive number or 0.\n";
+        }
+    
+    }while(students > people || students < 0);
+    
+        baseDiscount = discount(PERSON_PRICE, people);//find the discount.     
+    
+    return (PERSON_PRICE * people) + (INSTRUCRION_PRICE * students) - baseDiscount;
+}   // end of scuba()
+
 double skyDive(int people)
 {
     char housingChoice;
@@ -88,14 +221,14 @@ double skyDive(int people)
         
     baseCost = baseCharge * people;
     
-    cout << "\nTHANK YOU FOR CHOICING THE 4 DAY SKYDIVING PLAN\n"
-         << "HOPE YOU HAVE A GREAT TIME!!" << endl;
+    cout << "\nTHANK YOU FOR CHOOSING THE 4 DAY SKYDIVING PLAN!!!\n"
+         << "\tHOPE YOU HAVE A GREAT TIME!!" << endl;
         
     cout << "\nWhere would you like to stay? \n"
          << "Wilderness Lodge: $65/day per person \n"
-         << "Luxury Inn: $120/day per person\n"
+         << "Luxury Inn:       $120/day per person\n"
          << "Please enter in 'w' for Wilderness and 'l' for Luxury and for "
-         << "both enter in 'b'." << endl;
+         << "both enter in 'b': ";
     cin >> housingChoice;
                
     while (housingChoice != 'w' && housingChoice != 'l' && housingChoice != 'b') 
@@ -174,149 +307,8 @@ double skyDive(int people)
     cout << "Your housing cost is: $" << housingCost << endl;
     cout << "Your discount amount is: $" << allDiscount << endl;
     return (baseCost + housingCost - allDiscount);      
-}   // skidive()
+}   //end of skydive()
 
-//*******************************
-double climbing(int people)
-{
-    double 
-        discountTotal = 0,
-        baseTotal, 
-        totalAmount,  
-        climbOptional, 
-        equipOptional;
-        
-    int 
-        peopleOptional, 
-        daysOptional;
-
-    const double 
-        BASE_CHARGE = 350, 
-        DISCOUNT_RATE = 0.10, 
-        CLIMB_INSTR = 100, 
-        EQUIP_RENT = 40,
-        NUM_DAYS = 3;
-    
-    cout 
-        << "\nThank you for choosing Devil's Courthouse Adventure Weekend!" << endl
-        << endl << "Listed below are our rates." << endl
-        << "\t⇨ Base Charge: \t\t\t$" << BASE_CHARGE << " per person" << endl
-        << "\t⇨ Climbing Instructions: \t$" << CLIMB_INSTR << " per person (optional)" << endl
-        << "\t⇨ Equipment Rental: \t\t$" << EQUIP_RENT << "/day per person (optional)" << endl << endl;
-    
-    // calculate the base total, before discount
-    baseTotal = BASE_CHARGE * people;
-    
-    // call discount() to calculate the discount
-    discountTotal = discount(BASE_CHARGE, people);
-    
-    cout 
-        << "You have [" << people << "] people in your party." << endl
-        << "Your Base Charge (before discount) is:\t$" << baseTotal << endl
-        << "Your Discount Total is:\t\t\t(-$" << discountTotal << ")" << endl;
-        
-        baseTotal -= discountTotal; // subtract discount total from the base total
-        totalAmount = baseTotal;    // update the total amount
-    
-        
-    cout << "Your Base Charge (after discount) is:\t$" << baseTotal << endl << endl;
-    
-    cout << "Enter the number of people who will need a climbing instructor: ";
-    cin >> peopleOptional;
-    
-    while (peopleOptional < 0 || peopleOptional > people)
-        // the people who need instruction (peopleOptional) should not exceed the number of people in the party
-    {  
-        cout 
-            << "\tError: You entered more people than are in your party. Please try again."<< endl
-            << "Enter the number of people who will need a climbing instructor: ";
-        cin >> peopleOptional;
-    }   // while()
-        
-    
-    
-    cout
-        << "\n[" << peopleOptional << "] people will need a climbing instructor." << endl 
-        << "$" << CLIMB_INSTR << " charge x [" << peopleOptional << "] people =" << endl;
-    
-    climbOptional = CLIMB_INSTR * peopleOptional;   // the optional climbing instruction charge is $100/person
-    
-    cout << "\t⇨ $" << climbOptional << " will be added to your current total of $" << totalAmount << "." << endl;
-    totalAmount += climbOptional;  // add the optional charge of climbing instructions to the totalCharge
-    
-    cout << "\t⇨ Your updated total is $" << totalAmount << "." << endl << endl;
-    
-    cout << "Enter the number of people who will need to rent equipment: ";
-    cin >> peopleOptional;
-    
-    while (peopleOptional < 0 || peopleOptional > people)
-        // the people who need instruction (peopleOptional) should not exceed the number of people in the party
-    {
-        cout 
-            << "\tError: You entered more people than are in your party. Please try again." << endl
-            << "Enter the number of people who will need to rent equipment: ";
-        cin >> peopleOptional;
-    }   // while()
-        
-    cout << "How many days will you be renting equipment? ";
-    cin >> daysOptional;
-        
-    while (daysOptional < 0 || daysOptional >  NUM_DAYS)
-        // the number of days needed for equipment (daysOptional) should not exceed the total number of days
-    {   cout 
-            << "\tError: You entered more days than this package allows. Please try again." << endl
-            << "How many days will you be renting equipment? ";
-        cin >> daysOptional;
-    }   // while()
-        
-    
-    cout << "\n$" << EQUIP_RENT << " charge x [" << peopleOptional << "] people x [" << daysOptional << "] days =" << endl;
-    equipOptional = EQUIP_RENT * peopleOptional * daysOptional; // the optional equipment rental charge is $40/day per person
-
-    cout << "\t⇨ $" << equipOptional << " will be added to your current total of $" << totalAmount << "." << endl;
-
-    totalAmount +=  equipOptional;  // add the optional charge of equipment rental, equipOptional, to the totalCharge
-    
-    cout << "\t⇨ Your updated total is $" << totalAmount << "." << endl;
-    
-        
-    return totalAmount;    
-}   // climbing()
-
-//*******************************
-double scuba(int people)
-{
-    const double PERSON_PRICE = 350;//price per person
-    const double INSTRUCRION_PRICE = 100;//price of scuba instruction
-    int students;//variable to hold the number of people who need scuba instruction
-    double baseDiscount = 0;//discount off of the base price (positive number!)
-       
-    cout << "Thank you for choosing Scuba Bahama!\n";
-    do//check and make sure the amount of people who need instruction is <= the number of people going on the trip
-    {
-        cout << "How many people need diving lessons?\n";
-        cout << "(Those with prior experience may dive right in, while beginners should choose\n"; 
-        cout << "to take optional, but very affordable lessons.)\n";
-        cin >> students;
-    
-        if(students > people)
-        {
-            cout << "You have entered a number greater than the number of people in your party.\n";
-            cout << "Please enter a smaller number.\n\n";
-        }
-        else if(students < 0)
-        {
-            cout << "Please enter a positive number or 0.\n";
-        }
-    
-    }while(students > people || students < 0);
-    
-        baseDiscount = discount(PERSON_PRICE, people);//find the discount.     
-    
-    return (PERSON_PRICE * people) + (INSTRUCRION_PRICE * students) - baseDiscount;
-}   // scuba()
-
-//*******************************
 double spelunk(int people)
 {
     const double PERSON_PRICE = 700;//base price per person
@@ -326,7 +318,7 @@ double spelunk(int people)
     int days;//days that a single person is renting equipment for
     int totalDays = 0;//total days that equipment is getting rented for 
     
-    cout << "Thank you for choosing Barron Cliff Spelunk!\n";
+    cout << "\nThank you for choosing Barron Cliff Spelunk!\n";
     do//error checking for input
     {
         cout << "Optional equipment rental is $40 per day for every person.\n";
@@ -367,27 +359,11 @@ double spelunk(int people)
         }while((days > 8) || (days < 0));//continue until user enters valid input        
     }  
     return ((RENTAL * totalDays) + (people * PERSON_PRICE) - baseDiscount); 
-}   // spelunk
+}   // end of spelunk
 
-//*******************************
-void receipt(double total, int people, double discount)
-{
-    cout 
-        << "\nYour charges are as follows:" << endl
-        << "\t⇨ Per person: \t$" << total/people << endl
-        << "\t⇨ Total people: " << people << endl
-        << "Base Charge: \t\t$" << total + discount << endl
-        << "\t⇨ Discount: \t-($" << discount << ")" << endl
-        << "TOTAL CHARGES: \t\t$" << total << endl
-        << "\nThank you for purchasing your vacation package!" << endl;   
-}   // receipt()
-
-//*******************************
 double discount(char choice, int people)
 {
-
-    const double DISCOUNT_RATE = 0.10;  // 10% discount only applies to parties of 5 or more people
-        
+    const double DISCOUNT_RATE = 0.10;  // 10% discount only applies to parties of 5 or more people       
     double
         baseCharge, 
         totalDiscount = 0;
@@ -406,27 +382,24 @@ double discount(char choice, int people)
                 
             case '3': //Sky Dive Colorado
                 baseCharge = 700;
-                break;  
-                
+                break;   
+                               
             case '4': // Barron Cliff Spelunk
                 baseCharge = 700;
-                break;
-            
+                break;   
+                        
             default:
                 cout << "Invalid. Try again." << endl;
-                break;   
-                
+                break;                   
         }   // switch()
         
-        totalDiscount = baseCharge * DISCOUNT_RATE;
+        totalDiscount = (baseCharge * DISCOUNT_RATE) * people;
         return totalDiscount;
     }   // if()
-    
-    
+       
     return totalDiscount;
-}   // discount()
-  
-//*******************************
+}   //end of discount()
+
 double discount(double baseCharge, int people)
 {
     baseCharge = people * baseCharge;
@@ -438,5 +411,17 @@ double discount(double baseCharge, int people)
     {//if less than five people return 0
         return 0;
     }
-}   // discount()
+}   // end of discount()
+
+void receipt(double total, int people, double discount)
+{
+    cout 
+        << "\nYour charges are as follows:" << endl
+        << "\t⇨ Per person: \t$" << total/people << endl
+        << "\t⇨ Total people: " << people << endl
+        << "Base Charge: \t\t$" << total + discount << endl
+        << "\t⇨ Discount: \t-($" << discount << ")" << endl
+        << "TOTAL CHARGES: \t\t$" << total << endl
+        << "\nThank you for purchasing your vacation package!\n" << endl;   
+}   // end of receipt()
 
